6cd236c104f2fd55f325a69c4c552cfecf7319dc
sebteouii <sebteouiikhoubayb@gmail.com>
2024-03-31 15:02:06 +0200

    DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,87 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.review import Review
+from models.amenity import Amenity
+from os import getenv
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+"""many to many relationship"""
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
+
+class Place(BaseModel, Base):
+    """ Represents a Place for a MySQL database.
+
+    Attributes:
+        city_id (str): The ID of the city where the place is located.
+        user_id (str): The ID of the user who owns the place.
+        name (str): The name of the place.
+        description (str): A description of the place.
+        number_rooms (int): The number of rooms in the place.
+        number_bathrooms (int): The number of bathrooms in the place.
+        max_guest (int): The maximum number of guests.
+        price_by_night (int): The price per night for staying at the place.
+        latitude (float): The latitude coordinate of the place.
+        longitude (float): The longitude coordinate of the place.
+
+    Relationships:
+        reviews (relationship):
+            One-to-Many relationship with the Review class.
+        amenities (relationship):
+            Many-to-Many relationship with the Amenity class.
+    """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship(
+        "Review",
+        backref="place",
+        cascade="delete")
+    amenities = relationship(
+        "Amenity",
+        secondary="place_amenity",
+        viewonly=False,
+        back_populates="place_amenities"
+    )
+
     amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """ Getter attribute for reviews in FileStorage """
+            from models import storage
+            reviews_list = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
+
+        @property
+        def amenities(self):
+            """ Getter attribute for amenities in FileStorage """
+            from models import storage
+            return [storage.all(Amenity)[amenity_id]
+                    for amenity_id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, amenity):
+            """ Setter attribute for amenities in FileStorage """
+            if isinstance(amenity, Amenity):
+                self.amenity_ids.append(amenity.id)
