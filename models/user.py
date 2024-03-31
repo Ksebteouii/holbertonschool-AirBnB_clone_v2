1f9e42d9b0a41b4564769f17a8b265a2f085e546
sebteouii <sebteouiikhoubayb@gmail.com>
2024-03-31 14:47:24 +0200

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password associated with the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+
+    Relationships:
+        places (relationship): One-to-Many relationship with the Place class.
+        reviews (relationship): One-to-Many relationship with the Review class.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
